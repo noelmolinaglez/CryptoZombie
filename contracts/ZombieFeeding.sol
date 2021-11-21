0e61e130078a903e3688f945d856504470940ab6
Noel Molina <noelmolinaglez@gmail.com>
2021-11-21 18:54:44 -0500

    Adding kitty feature.

--- contracts/ZombieFeeding.sol
+++ contracts/ZombieFeeding.sol
@@ -24,11 +24,14 @@
   KittyInterface kittyContract = KittyInterface(ckAddress);
 
 
-  function feedAndMultiply(uint _zombieId, uint _targetDna) public {
+  function feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) public {
     require(msg.sender == zombieToOwner[_zombieId]);
     Zombie storage myZombie = zombies[_zombieId];
     _targetDna = _targetDna % dnaModulus;
     uint newDna = (myZombie.dna + _targetDna) / 2;
+    if (keccak256(abi.encodePacked(_species)) == keccak256(abi.encodePacked("kitty"))) {
+       newDna = newDna - newDna % 100 +99;
+    }
     _createZombie("NoName", newDna); 
 
   }
@@ -36,7 +39,7 @@
    function feedOnKitty(uint _zombieId, uint _kittyId) public {
     uint kittyDna;
     (,,,,,,,,,kittyDna) = kittyContract.getKitty(_kittyId);
-    feedAndMultiply(_zombieId, kittyDna);
+    feedAndMultiply(_zombieId, kittyDna, "kitty");
   }
 
  } 
