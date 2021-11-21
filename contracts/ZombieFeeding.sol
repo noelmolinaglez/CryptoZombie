f0b9836fcc503540bf58b14cea768e1b26dd1b61
Noel Molina <noelmolinaglez@gmail.com>
2021-11-20 21:43:22 -0500

    Implenting feedAndMultiply function.

--- contracts/ZombieFeeding.sol
+++ contracts/ZombieFeeding.sol
@@ -5,6 +5,11 @@
 
  contract ZombieFeeding  is ZombieFactory {
     
-    
+    function feedAndMultiply(uint _zombieId, uint _targetDna) public {
+    require(msg.sender == zombieToOwner[_zombieId]);
+    Zombie storage myZombie = zombies[_zombieId];
+   
+  }
+
 
  } 
